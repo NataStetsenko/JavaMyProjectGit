a0fb84756a6f7b636994f9e546acf065521f216f
Natali <1979stetsenko@gmail.com>
2023-07-12 15:49:40 +0300

    Commit2

---
+++ src/main/java/step/learning/control/ControlDemo.java
@@ -0,0 +1,35 @@
+package step.learning.control;
+
+public class ControlDemo {
+    public void run(){
+        System.out.println("ControlDemo run");
+//        new HomeWork1().print();
+        //Примитиви
+        byte xb;
+        short xs;
+        int xi;
+        long xl;
+        float xf;
+        double xd;
+        boolean xbo;
+        char cx;
+        //З примитивами можуть вмникнути проблеми при створенні колекцій
+        //Посилальні типи
+        Byte b = 1;
+        Short s = 2;
+        Integer i = 3;
+        Long l = 4L;
+        Float f = 0.01F;
+        Double d = 0.01;
+        Boolean bo = true;
+        Character x = 'A';
+        //Масиви
+        int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int arr2[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int arr3[]= new int[5];
+        /*
+        * Control flow instructions - шнструкції управлінням виконанням
+        * операторів умовного та ціклічного виконання, а також переходів між інструкціями
+        *  */
+    }
+}
